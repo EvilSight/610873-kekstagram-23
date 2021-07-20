357ff7a4b2da59fd1ef6984bc0acb1ea03439687
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-07-21 00:53:11 +0300

    Добавляет правки

--- js/photo-viewer.js
+++ js/photo-viewer.js
@@ -31,6 +31,36 @@ const createCommentElem = ({avatar, name, message}) =>{
   return commentElement;
 };
 
+const renderPhoto = ({url, description, likes}) => {
+    if(imgElement){
+      imgElement.src = url;
+      imgElement.alt = description;
+    }
+  
+    if(captionElement){
+      captionElement.textContent = description;
+    }
+  
+    if(likesElement){
+      likesElement.textContent = likes;
+    }
+  };
+  
+  const renderComments = (comments) => {
+  
+    if(commentsElement){
+      commentsElement.innerHTML ='';
+      const commentsFragment = document.createDocumentFragment();
+  
+      comments.forEach((comment) => commentsFragment.appendChild(createCommentElem(comment)));
+      commentsElement.appendChild(commentsFragment);
+    }
+  
+    if(commentsCountElement){
+      commentsCountElement.textContent = comments.length;
+    }
+  };
+  
 const onPhotoViewerCloseClick = () => {
     // eslint-disable-next-line no-use-before-define
     hidePhoto();
@@ -45,31 +75,32 @@ const onPhotoViewerEscKeydown = () => {
 };
 
 const hitePhoto = () => {
-    bigPictureElement.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    document.removeEventListener('keydown', onPhotoViewerEscKeydown);
-    btnPictureCancelElement.removeEventListener('click', onPhotoViewerCloseClick);
+    if(bigPictureElement){
+        bigPictureElement.classList.add('hidden');
+        document.body.classList.remove('modal-open');
+        document.removeEventListener('keydown', onPhotoViewerEscKeydown);
+    
+        if(btnPictureCancelElement){
+          btnPictureCancelElement.removeEventListener('click', onPhotoViewerCloseClick);
+        }
+      }
 }; 
 
 
 const showPhoto = (photoDescription) => {
 
-    document.body.classList.add('modal-open');
-  bigPictureElement.classList.remove('hidden');
-  document.addEventListener('keydown', onPhotoViewerEscKeydown);
-  btnPictureCancelElement.addEventListener('click', onPhotoViewerCloseClick);
-
-  imgElement.src = photoDescription.url;
-  imgElement.alt = photoDescription.description;
-  captionElement.textContent = photoDescription.description;
-  likesElement.textContent = photoDescription.likes;
-  commentsCountElement.textContent = photoDescription.comments.length;
-
-  commentsElement.innerHTML ='';
-  const commentsFragment = document.createDocumentFragment();
-
-  photoDescription.comments.forEach((comment) => commentsFragment.appendChild(createCommentElem(comment)));
-  commentsElement.appendChild(commentsFragment);
-};
+    if(bigPictureElement){
+      document.body.classList.add('modal-open');
+      bigPictureElement.classList.remove('hidden');
+  
+      document.addEventListener('keydown', onPhotoViewerEscKeydown);
+      if(btnPictureCancelElement){
+        btnPictureCancelElement.addEventListener('click', onPhotoViewerCloseClick);
+      }
+  
+      renderPhoto(photoDescription);
+      renderComments(photoDescription.comments);
+    }
+  };
 
 export {showPhoto};
