4f8bf5c2b7bfd0c7bd3398dd1be224dc6e616f6e
Chibisov Dmitriy <demonzgrad@mail.ru>
2021-07-21 00:44:10 +0300

    Добавляет возможность просмотра фото в fullsize-режиме

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -2,10 +2,20 @@ import { renderPictures } from './thumbnails-viewer.js';
 import {showPhoto} from './photo-viewer.js';
 import { createPhotoDescriptionList } from './data.js';
 
+const getShowPhotoСlosure = (photoDescription) => () => showPhoto(photoDescription);
+
+const addThumbnailClickHandler = (thumbnail, photoDescription) => {
+  thumbnail.addEventListener('click', getShowPhotoСlosure(photoDescription));
+};
+
 const createPhotoGallery = (createPhotoDescriptionList) => {
   if (photoDescriptionList && photoDescriptionList.length > 0) {
-    showPhoto(photoDescriptionList[0]);
     renderPictures(photoDescriptionList);
+
+    const thumbnails = document.querySelectorAll('.picture__img');
+    for (let i = 0; i < thumbnails.length; i++) {
+      addThumbnailClickHandler(thumbnails[i], photoDescriptionList[i]);
+    }
   }
 };
 
